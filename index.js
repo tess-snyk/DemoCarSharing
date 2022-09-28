b785c3f68e93f9a9ea252ccbcc0bfdb96177dac3
Tessa Davis <tess.davis@snyk.io>
2022-09-29 11:12:08 +1300

    new_api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('./config/VTSconfig.js');
-const vehicleDb = require('./dbInterface/vehicleDB_test.js');
-//const vehicleDb = require('./dbInterface/vehicleDb.js');
+//const vehicleDb = require('./dbInterface/vehicleDB_test.js');
+const vehicleDb = require('./dbInterface/vehicleDb.js');
 
 const app = express();
 
